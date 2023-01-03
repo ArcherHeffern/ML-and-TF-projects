ddad496b18d6248be16aa1e542d29e1869ff8f97
Archer <heffernarcher@gmail.com>
2023-01-02 19:47:42 -0500

    improved reliability and readiability of route

--- hostMLModel/main.py
+++ hostMLModel/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from model import load_model
 
 # TODO: Set up ssl
@@ -13,16 +13,18 @@ def index():
 
 @app.route("/predict", methods=['GET'])
 def predict():
-    if request.headers.get('Content-Type') == 'application/json':
-        celcius = request.get_json().get('data')
-        if celcius == None:
-            print(celcius)
-            result = model.predict([celcius])
-            return Response({'temperature':result[0]}, 200, mimetype='text/plain')
-        else:
-            return Response("Did not have celcius parameter in body", 400, mimetype='text/plain')
-    else:
+    # security?
+    # must ensure: body is json, body contains celcius parameter, celcius is a float/int
+    if request.headers.get('Content-Type') != 'application/json':
         return Response("Content type not application/json", 400)
+    celcius = request.get_json().get('data')
+    if celcius == None or float != type(celcius) != int:
+        return Response("Did not have celcius parameter in body or was not a float/int", 400, mimetype='text/plain')
+    else:
+        result = model.predict([celcius])
+        result = {"temperature": float(result[0][0])}
+        return jsonify(result)
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
